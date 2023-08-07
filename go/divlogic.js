1e102e287cb55fb5d8ed61887b7a11eb0ba4c1a4
Thecartoondude <141502127+Thecartoondude@users.noreply.github.com>
2023-08-07 08:59:53 -0400

    Create divlogic.js

---
+++ go/divlogic.js
@@ -0,0 +1,59 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// resize fix for ns4
+var origWidth, origHeight;
+if (document.layers) {
+        origWidth = window.innerWidth; origHeight = window.innerHeight;
+        window.onresize = function() { if (window.innerWidth != origWidth || window.innerHeight != origHeight) history.go(0); }
+}
+
+
+function getElemRefs(id) {
+        var el = (document.getElementById)? document.getElementById(id): (document.all)? document.all[id]: (document.layers)? getLyrRef(id,document): null;
+        if (el) el.css = (el.style)? el.style: el;
+        return el;
+}
+
+
+
+// dhtmllib.js Mike Hall  www.brainjar.com
+function getLyrRef(lyr,doc) {
+        if (document.layers) {
+                var theLyr;
+                for (var i=0; i<doc.layers.length; i++) {
+                theLyr = doc.layers[i];
+                        if (theLyr.name == lyr) return theLyr;
+                        else if (theLyr.document.layers.length > 0)
+                if ((theLyr = getLyrRef(lyr,theLyr.document)) != null)
+                                        return theLyr;
+          }
+                return null;
+  }
+}
+
+var current_visible_id;
+
+function rolloverdiv(arg) {
+        if (current_visible_id) {
+                var oldbox = getElemRefs(current_visible_id);
+                if (oldbox.css) {
+                        oldbox.css.visibility = "hidden";
+                }
+        }
+        var newbox = getElemRefs(arg);
+        if (newbox.css) {
+                newbox.css.visibility = "visible";
+        }
+	current_visible_id = arg;
+}
+
