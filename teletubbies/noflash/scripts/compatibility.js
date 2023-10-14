639f9cf0ab4f1f146b4e782894cf176065647657
foxtrot5f3 <141502127+Thecartoondude@users.noreply.github.com>
2023-10-13 20:21:34 -0400

    Create compatibility.js

---
+++ teletubbies/noflash/scripts/compatibility.js
@@ -0,0 +1,12 @@
+/**
+* Interactive Game Compatibility Script
+* Version 1.0
+* Copyright Ragdoll Ltd.
+*/
+
+setMinimumRequiredBrowserVersion(EXPLORER, 4.0);
+setMinimumRequiredBrowserVersion(NETSCAPE, 4.0);
+setMinimumRequiredBrowserVersion(OPERA, 7.0);
+setMinimumRequiredBrowserVersion(MOZILLA, 1.6);
+
+}
