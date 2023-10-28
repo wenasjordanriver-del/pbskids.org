2e32ecfc28d5c7d2d9ea8521e9c02fefc7f9485d
foxtrot5f3s_hideout <141502127+Thecartoondude@users.noreply.github.com>
2023-10-28 11:17:51 -0400

    Update go.js

--- go/go.js
+++ go/go.js
@@ -1,19 +1,19 @@
 <!-- Hide from older browsers
 
 var minibox_off = new Image();
-minibox_off.src = "/PBS-Kids-2006/images/home-minibox-off.gif";
+minibox_off.src = "/PBS-Kids-2006/go/images/home-minibox-off.gif";
 var minibox_on = new Image();
-minibox_on.src = "/PBS-Kids-2006/images/home-minibox-on.gif";
+minibox_on.src = "/PBS-Kids-2006/go/images/home-minibox-on.gif";
 
 var preschool_off = new Image();
-preschool_off.src = "/PBS-Kids-2006/images/home-preschool-off.gif";
+preschool_off.src = "/PBS-Kids-2006/go/images/home-preschool-off.gif";
 var preschool_on = new Image();
-preschool_on.src = "/PBS-Kids-2006/images/home-preschool-on.gif";
+preschool_on.src = "/PBS-Kids-2006/go/images/home-preschool-on.gif";
 
 var figuredog_off = new Image();
-figuredog_off.src = "/PBS-Kids-2006/images/figure-dog-off.gif";
+figuredog_off.src = "/PBS-Kids-2006/go/images/figure-dog-off.gif";
 var figuredog_on = new Image();
-figuredog_on.src = "/PBS-Kids-2006/images/figure-dog-on.gif";
+figuredog_on.src = "/PBS-Kids-2006/go/images/figure-dog-on.gif";
 
 function activate(imgName) {
   if ( eval(imgName + "_on.complete") ) {
