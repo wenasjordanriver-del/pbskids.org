c2e541cba2ebdd6161b00a43facc070beaf27b57
foxtrot5f3s_hideout <141502127+Thecartoondude@users.noreply.github.com>
2023-11-02 18:28:02 -0400

    Create signup.pl

---
+++ cgi-registry/authentication/signup.pl
@@ -0,0 +1,78 @@
+
+
+<title>My PBS KIDS GO! | PBS KIDS</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="/PBS-Kids-2006/mypbskids/css/preferences.css">
+<link rel="stylesheet" type="text/css" href="/PBS-Kids-2006/mypbskids/css/preferences-themegreen.css">
+<link rel="stylesheet" type="text/css" href="/PBS-Kids-2006/mypbskids/css/preferences-login.css">
+</head>
+
+<body>
+ 
+<!-- HEADER -->
+<div class="pageheader">
+<a href="/PBS-Kids-2006/go/"><img src="/PBS-Kids-2006/mypbskids/images/logo-go-themegreen.gif" border="0" width="93" height="80" alt="PBS KIDS GO!"/></a><img src="/PBS-Kids-2006/mypbskids/images/header-titlesignup-themegreen.gif" border="0" width="477" height="80" alt="Sign up"/><a href="javascript:history.go(-1)"><img src="/PBS-Kids-2006/mypbskids/images/back-themegreen.gif" border="0" height="80" width="80" alt="back"/></a></div>
+<!-- END HEADER -->
+<!-- CONTENT -->
+
+
+<table border="0" cellpadding="0" cellspacing="20" width="650" class="mainbox">
+<tr>
+<td>
+<a href="/PBS-Kids-2006/mypbskids"><img src="/PBS-Kids-2006/images/programs/small-mypbskids.gif" width="95" height="50" border="0" class="showlogo"></a>
+<div class="welcomesmall">With a PBS KIDS GO! username and password, you can create your very own page, where you choose your background color and your favorite games. You can also keep track of your &lt;a href=&#39;/eekoworld&#39;&gt;EekoWorld&lt;/a&gt; and &lt;a href=&#39;/fetch&#39;&gt;Fetch&lt;/a&gt; points, and cast votes for your favorite &lt;a href=&#39;/cyberchase&#39;&gt;Cyberchase&lt;/a&gt; games.</div>
+<div class=""></div></td>
+</tr>
+<tr>
+<td><div class="boxhuge">
+<form class="createform" action="/PBS-Kids-2006/cgi-registry/authentication/signup.pl" method="post">
+<input type="hidden" name="site" value="Kids">
+<input type="hidden" name="destination" value="/PBS-Kids-2006/mypbskids">
+<input type="hidden" name="activity" value="/PBS-Kids-2006/mypbskids">
+<input type="hidden" name="activity_path" value="/PBS-Kids-2006/mypbskids">
+<input type="hidden" name="image" value="mypbskids">
+<input type="hidden" name="parentsemail" value="webtech@pbs.org">
+<input type="hidden" name="create" value="1">
+<div class="signupbig">Create a username:</div>
+<input name="username" type="text" size="25" maxlength="12"/>
+<div class="signupnote">Don't use your real name. Make it between 3 and 12 characters long and use letters and numbers only (no symbols or spaces).</div>
+<div class="signupbig">Create a password:</div>
+<input name="password1" type="password" size="25" maxlength="16"/>
+<div class="signupbig">Re-type password:</div>
+<input name="password2" type="password" size="25" maxlength="16"/>
+<div class="signupnote">It should be between 3 and 12 characters long and contain no spaces.</div>
+<div class="signupbig">Pick a secret question:</div>
+<select name="hintquestion">
+<option value="What's your pet's name?">What's your pet's name?</option>
+<option value="Where were you born?">Where were you born?</option>
+<option value="What's your kindergarten teacher's name?">What's your kindergarten teacher's name?</option>
+<option value="What's your favorite animal?">What's your favorite animal?</option>
+<option value="What's your favorite school subject?">What's your favorite school subject?</option>
+<option value="What's your favorite TV show?">What's your favorite TV show?</option>
+</select>
+<div class="signupbig">Type the answer:</div>
+<input name="hintanswer" type="text" size="30" maxlength="250" value=""/>
+<div class="signupnote">If you forget your password, we'll ask you this question to make sure you are really you.</div>
+<input type="image" src="/PBS-Kids-2006/mypbskids/images/button-createmyaccount-themegreen.gif" width="300" height="54" border="0" alt="Create my account" value="Signup">
+</form>
+</div>
+</td>
+</tr>
+</table>
+<br/>
+
+
+<!-- END CONTENT -->
+
+<!-- END CONTENT -->
+<div class="fineprint" align="center">
+
+<a href="/PBS-Kids-2006/tvschedules/" class="fineprint">TV Times</a> | <a href="/PBS-Kids-2006/help/" class="fineprint">Help</a><br>
+<a href="/PBS-Kids-2006/" class="fineprint">PBS KIDS</a> | <a href="/PBS-Kids-2006/go/" class="fineprint">PBS KIDS GO!</a><br><br>
+Copyright &copy; 1995-2013 Public Broadcasting Service (PBS). All rights reserved.</div>
+<!-- END FOOTER -->
+
+
+</body>
+
+</html>
