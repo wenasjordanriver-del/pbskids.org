bf29abfdeb9da6868f4282b70e928ca19003e933
foxtrot5f3s_hideout <141502127+Thecartoondude@users.noreply.github.com>
2023-11-09 07:54:20 -0500

    Update flashobject.js

--- flashobject.js
+++ flashobject.js
@@ -1,301 +1,140 @@
 /**
- * SWFObject v1.5: Flash Player detection and embed - http://blog.deconcept.com/swfobject/
+ * FlashObject v1.3b: Flash detection and embed - http://blog.deconcept.com/flashobject/
  *
- * SWFObject is (c) 2007 Geoff Stearns and is released under the MIT License:
+ * FlashObject is (c) 2006 Geoff Stearns and is released under the MIT License:
  * http://www.opensource.org/licenses/mit-license.php
  *
  */
-if(typeof deconcept=="undefined"){var deconcept=new Object();}
-if(typeof deconcept.util=="undefined"){deconcept.util=new Object();}
-if(typeof deconcept.SWFObjectUtil=="undefined"){
-	deconcept.SWFObjectUtil=new Object();
-	}
-deconcept.SWFObject=function(_1,id,w,h,_5,c,_7,_8,_9,_a){
-	if(!document.getElementById){
-	return;
-		}
-	this.DETECT_KEY=_a?_a:"detectflash";
-	this.skipDetect=deconcept.util.getRequestParameter(this.DETECT_KEY);
-	this.params=new Object();
-	this.variables=new Object();
-	this.attributes=new Array();
-		if(_1){
-			this.setAttribute("swf",_1);
-			}
-		if(id){
-			this.setAttribute("id",id);
-			}
-		if(w){
-			this.setAttribute("width",w);
-			}
-		if(h){
-			this.setAttribute("height",h);
-			}
-		if(_5){
-			this.setAttribute("version",new deconcept.PlayerVersion(_5.toString().split(".")));
-			}
-	this.installedVer=deconcept.SWFObjectUtil.getPlayerVersion();
-		if(!window.opera&&document.all&&this.installedVer.major>7){
-			deconcept.SWFObject.doPrepUnload=true;
-			}
-		if(c){
-			this.addParam("bgcolor",c);
-			}
-	var q=_7?_7:"high";
-	this.addParam("quality",q);
-	this.setAttribute("useExpressInstall",false);
-	this.setAttribute("doExpressInstall",false);
-	var _c=(_8)?_8:window.location;
-	this.setAttribute("xiRedirectUrl",_c);
-	this.setAttribute("redirectUrl","");
-		if(_9){
-			this.setAttribute("redirectUrl",_9);
-			}
-		};
-
-deconcept.SWFObject.prototype={
-	useExpressInstall:function(_d){
-		this.xiSWFPath=!_d?"expressinstall.swf":_d;
-		this.setAttribute("useExpressInstall",true);
-	},setAttribute:function(_e,_f){
-		this.attributes[_e]=_f;
-	},getAttribute:function(_10){
-		return this.attributes[_10];
-	},addParam:function(_11,_12){
-		this.params[_11]=_12;
-	},getParams:function(){
-		return this.params;
-	},addVariable:function(_13,_14){
-		this.variables[_13]=_14;
-	},getVariable:function(_15){
-		return this.variables[_15];
-	},getVariables:function(){
-		return this.variables;
-	},getVariablePairs:function(){
-		var _16=new Array();
-		var key;
-		var _18=this.getVariables();
-		for(key in _18){
-			_16[_16.length]=key+"="+_18[key];
-		}
-		return _16;
-	},getSWFHTML:function(){
-		var _19="";
-		if(navigator.plugins&&navigator.mimeTypes&&navigator.mimeTypes.length){
-			if(this.getAttribute("doExpressInstall")){
-				this.addVariable("MMplayerType","PlugIn");
-				this.setAttribute("swf",this.xiSWFPath);
-			}
-			_19="<embed type=\"application/x-shockwave-flash\" src=\""+this.getAttribute("swf")+"\" width=\""+this.getAttribute("width")+"\" height=\""+this.getAttribute("height")+"\" style=\""+this.getAttribute("style")+"\"";_19+=" id=\""+this.getAttribute("id")+"\" name=\""+this.getAttribute("id")+"\" ";
-			var _1a=this.getParams();
-				for(var key in _1a){
-					_19+=[key]+"=\""+_1a[key]+"\" ";
-				}
-			var _1c=this.getVariablePairs().join("&");
-				if(_1c.length>0){
-					_19+="flashvars=\""+_1c+"\"";
-				}
-			_19+="/>";
-		}else{
-			if(this.getAttribute("doExpressInstall")){
-				this.addVariable("MMplayerType","ActiveX");
-				this.setAttribute("swf",this.xiSWFPath);
-			}
-			_19="<object id=\""+this.getAttribute("id")+"\" classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" width=\""+this.getAttribute("width")+"\" height=\""+this.getAttribute("height")+"\" style=\""+this.getAttribute("style")+"\">";_19+="<param name=\"movie\" value=\""+this.getAttribute("swf")+"\" />";
-			var _1d=this.getParams();
-			for(var key in _1d){
-				_19+="<param name=\""+key+"\" value=\""+_1d[key]+"\" />";
-			}
-			var _1f=this.getVariablePairs().join("&");
-			if(_1f.length>0){_19+="<param name=\"flashvars\" value=\""+_1f+"\" />";
-			}
-			_19+="</object>";
-		}
-	return _19;
-},write:function(_20){
-	if(this.getAttribute("useExpressInstall")){
-		var _21=new deconcept.PlayerVersion([6,0,65]);
-			if(this.installedVer.versionIsValid(_21)&&!this.installedVer.versionIsValid(this.getAttribute("version"))){
-				this.setAttribute("doExpressInstall",true);
-				this.addVariable("MMredirectURL",escape(this.getAttribute("xiRedirectUrl")));
-				document.title=document.title.slice(0,47)+" - Flash Player Installation";
-				this.addVariable("MMdoctitle",document.title);
-			}
-	}
-	if(this.skipDetect||this.getAttribute("doExpressInstall")||this.installedVer.versionIsValid(this.getAttribute("version"))){
-		var n=(typeof _20=="string")?document.getElementById(_20):_20;
-		n.innerHTML=this.getSWFHTML();
-		return true;
-	}else{
-		if(this.getAttribute("redirectUrl")!=""){
-			document.location.replace(this.getAttribute("redirectUrl"));
-		}
-	}
-	return false;
-	}
+if(typeof com=="undefined"){var com=new Object();}
+if(typeof com.deconcept=="undefined"){com.deconcept=new Object();}
+if(typeof com.deconcept.util=="undefined"){com.deconcept.util=new Object();}
+if(typeof com.deconcept.FlashObjectUtil=="undefined"){com.deconcept.FlashObjectUtil=new Object();}
+com.deconcept.FlashObject=function(_1,id,w,h,_5,c,_7,_8,_9,_a,_b){
+if(!document.createElement||!document.getElementById){return;}
+this.DETECT_KEY=_b?_b:"detectflash";
+this.skipDetect=com.deconcept.util.getRequestParameter(this.DETECT_KEY);
+this.params=new Object();
+this.variables=new Object();
+this.attributes=new Array();
+this.useExpressInstall=_7;
+if(_1){this.setAttribute("swf",_1);}
+if(id){this.setAttribute("id",id);}
+if(w){this.setAttribute("width",w);}
+if(h){this.setAttribute("height",h);}
+if(_5){this.setAttribute("version",new com.deconcept.PlayerVersion(_5.toString().split(".")));}
+this.installedVer=com.deconcept.FlashObjectUtil.getPlayerVersion(this.getAttribute("version"),_7);
+if(c){this.addParam("bgcolor",c);}
+var q=_8?_8:"high";
+this.addParam("quality",q);
+var _d=(_9)?_9:window.location;
+this.setAttribute("xiRedirectUrl",_d);
+this.setAttribute("redirectUrl","");
+if(_a){this.setAttribute("redirectUrl",_a);}
 };
-
-deconcept.SWFObjectUtil.getPlayerVersion=function(){
-	var _23=new deconcept.PlayerVersion([0,0,0]);
-		if(navigator.plugins&&navigator.mimeTypes.length){
-			var x=navigator.plugins["Shockwave Flash"];
-				if(x&&x.description){
-					_23=new deconcept.PlayerVersion(x.description.replace(/([a-zA-Z]|\s)+/,"").replace(/(\s+r|\s+b[0-9]+)/,".").split("."));
-				}
-		}else{
-			if(navigator.userAgent&&navigator.userAgent.indexOf("Windows CE")>=0){
-				var axo=1;
-				var _26=3;
-					while(axo){
-						try{_26++;
-						axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash."+_26);
-						_23=new deconcept.PlayerVersion([_26,0,0]);
-						}
-					catch(e){
-						axo=null;
-					}
-					}
-			}else{
-				try{
-					var axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");
-				}
-				catch(e){
-					try{
-						var axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash.6");
-						_23=new deconcept.PlayerVersion([6,0,21]);
-						axo.AllowScriptAccess="always";
-					}
-				catch(e){
-					if(_23.major==6){
-						return _23;
-					}
-				}
-				try{
-					axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
-				}
-				catch(e){
-				}
-				}
-				if(axo!=null){
-					_23=new deconcept.PlayerVersion(axo.GetVariable("$version").split(" ")[1].split(","));
-				}
-			}
-		}
-		return _23;
-};
-
-deconcept.PlayerVersion=function(_29){
-	this.major=_29[0]!=null?parseInt(_29[0]):0;
-	this.minor=_29[1]!=null?parseInt(_29[1]):0;
-	this.rev=_29[2]!=null?parseInt(_29[2]):0;
-};
-
-deconcept.PlayerVersion.prototype.versionIsValid=function(fv){
-	if(this.major<fv.major){
-		return false;
-	}
-	if(this.major>fv.major){
-		return true;
-	}
-	if(this.minor<fv.minor){
-		return false;
-	}
-	if(this.minor>fv.minor){
-		return true;
-	}
-	if(this.rev<fv.rev){
-		return false;
-	}
-	return true;
-};
-
-deconcept.util={getRequestParameter:function(_2b){
-	var q=document.location.search||document.location.hash;
-		if(_2b==null){
-			return q;
-		}
-		if(q){
-			var _2d=q.substring(1).split("&");
-				for(var i=0;i<_2d.length;i++){
-					if(_2d[i].substring(0,_2d[i].indexOf("="))==_2b){
-						return _2d[i].substring((_2d[i].indexOf("=")+1));
-					}
-				}
-		}
-		return "";}};
-
-deconcept.SWFObjectUtil.cleanupSWFs=function(){
-	var _2f=document.getElementsByTagName("OBJECT");
-		for(var i=_2f.length-1;i>=0;i--){
-			_2f[i].style.display="none";
-				for(var x in _2f[i]){
-					if(typeof _2f[i][x]=="function"){
-						_2f[i][x]=function(){};
-					}
-				}
-		}
-};
-	if(deconcept.SWFObject.doPrepUnload){
-		if(!deconcept.unloadSet){
-			deconcept.SWFObjectUtil.prepUnload=function(){
-				__flash_unloadHandler=function(){};
-				__flash_savedUnloadHandler=function(){};
-				window.attachEvent("onunload",deconcept.SWFObjectUtil.cleanupSWFs);
-			};
-		window.attachEvent("onbeforeunload",deconcept.SWFObjectUtil.prepUnload);
-		deconcept.unloadSet=true;
-		}
-	}
-	if(!document.getElementById&&document.all){
-		document.getElementById=function(id){
-			return document.all[id];
-		};
-	}
-var getQueryParamValue=deconcept.util.getRequestParameter;
-var FlashObject=deconcept.SWFObject;
-var SWFObject=deconcept.SWFObject;
-// start of PBS-specific functions
-
-
-/* PBS_get_mirror_base()
- * takes the current window's location and
- * returns an appropriate base url to use
- * for objects coming from our content mirrors
- * (currently hostnames appended with '-tc')
-*/ 
-function PBS_get_mirror_base() {
-  var protocol = window.location.protocol;
-  var host = window.location.host;
-  var pathname = window.location.pathname;
-
-  if (!pathname.match(/\/$/)) {
-    pathname = pathname.replace(/\/[^/]*$/, '/');
-  }
-
-  if (!pathname.match(/^\/_\//)) {
-    pathname = '' + pathname;
-  }
-
-  var base = protocol + '//' + host + pathname;
-  return base;
+com.deconcept.FlashObject.prototype={setAttribute:function(_e,_f){
+this.attributes[_e]=_f;
+},getAttribute:function(_10){
+return this.attributes[_10];
+},addParam:function(_11,_12){
+this.params[_11]=_12;
+},getParams:function(){
+return this.params;
+},addVariable:function(_13,_14){
+this.variables[_13]=_14;
+},getVariable:function(_15){
+return this.variables[_15];
+},getVariables:function(){
+return this.variables;
+},createParamTag:function(n,v){
+var p=document.createElement("param");
+p.setAttribute("name",n);
+p.setAttribute("value",v);
+return p;
+},getVariablePairs:function(){
+var _19=new Array();
+var key;
+var _1b=this.getVariables();
+for(key in _1b){_19.push(key+"="+_1b[key]);}
+return _19;
+},getFlashHTML:function(){
+var _1c="";
+if(navigator.plugins&&navigator.mimeTypes&&navigator.mimeTypes.length){
+if(this.getAttribute("doExpressInstall")){
+this.addVariable("MMplayerType","PlugIn");
 }
-
-/* PBS_get_mirror_url(path)
- * takes a relative url
- * and returns an appropriate url to use
- * for content to be retrieved from mirrors
- * (currently prepending the final file segment with '/mii')
-*/ 
-function PBS_get_mirror_url(path) {
-  if ( !path ) {
-    return undefined;
-  }
-  var segments = path.split('\/');
-  var file = segments.pop();
-  segments.push('enablecdn');
-  return ( segments.join('/') + '/' + file );
-}
-
-function PBS_enable_cdn(swfobj) {
-    swfobj.addParam('base', PBS_get_mirror_base());
-    swfobj.setAttribute('swf', PBS_get_mirror_url( swfobj.getAttribute('swf') ) );
+_1c="<embed type=\"application/x-shockwave-flash\" src=\""+this.getAttribute("swf")+"\" width=\""+this.getAttribute("width")+"\" height=\""+this.getAttribute("height")+"\"";
+_1c+=" id=\""+this.getAttribute("id")+"\" name=\""+this.getAttribute("id")+"\" ";
+var _1d=this.getParams();
+for(var key in _1d){_1c+=[key]+"=\""+_1d[key]+"\" ";}
+var _1f=this.getVariablePairs().join("&");
+if(_1f.length>0){_1c+="flashvars=\""+_1f+"\"";}
+_1c+="/>";
+}else{
+if(this.getAttribute("doExpressInstall")){this.addVariable("MMplayerType","ActiveX");}
+_1c="<object id=\""+this.getAttribute("id")+"\" classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" width=\""+this.getAttribute("width")+"\" height=\""+this.getAttribute("height")+"\">";
+_1c+="<param name=\"movie\" value=\""+this.getAttribute("swf")+"\" />\"";
+var _20=this.getParams();
+for(var key in _20){_1c+="<param name=\""+key+"\" value=\""+_20[key]+"\">";}
+var _22=this.getVariablePairs().join("&");
+if(_22.length>0){_1c+="<param name=\"flashvars\" value=\""+_22+"\">";
+_1c+="</object>";}}
+return _1c;
+},write:function(_23){
+if(this.useExpressInstall){
+var _24=new com.deconcept.PlayerVersion([6,0,65]);
+if(this.installedVer.versionIsValid(_24)&&!this.installedVer.versionIsValid(this.getAttribute("version"))){
+this.setAttribute("doExpressInstall",true);
+this.addVariable("MMredirectURL",escape(this.getAttribute("xiRedirectUrl")));
+document.title=document.title.slice(0,47)+" - Flash Player Installation";
+this.addVariable("MMdoctitle",document.title);}
+}else{this.setAttribute("doExpressInstall",false);}
+if(this.skipDetect||this.getAttribute("doExpressInstall")||this.installedVer.versionIsValid(this.getAttribute("version"))){
+var n=(typeof _23=="string")?document.getElementById(_23):_23;
+n.innerHTML=this.getFlashHTML();
+}else{if(this.getAttribute("redirectUrl")!=""){document.location.replace(this.getAttribute("redirectUrl"));}}}};
+com.deconcept.FlashObjectUtil.getPlayerVersion=function(_26,_27){
+var _28=new com.deconcept.PlayerVersion(0,0,0);
+if(navigator.plugins&&navigator.mimeTypes.length){
+var x=navigator.plugins["Shockwave Flash"];
+if(x&&x.description){_28=new com.deconcept.PlayerVersion(x.description.replace(/([a-z]|[A-Z]|\s)+/,"").replace(/(\s+r|\s+b[0-9]+)/,".").split("."));}
+}else{
+try{var axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
+for(var i=3;axo!=null;i++){
+axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash."+i);
+_28=new com.deconcept.PlayerVersion([i,0,0]);}}
+catch(e){}
+if(_26&&_28.major>_26.major){return _28;}
+if(!_26||((_26.minor!=0||_26.rev!=0)&&_28.major==_26.major)||_28.major!=6||_27){
+try{
+_28=new com.deconcept.PlayerVersion(axo.GetVariable("$version").split(" ")[1].split(","));
+}catch(e){}}}
+return _28;
+};
+com.deconcept.PlayerVersion=function(_2c){
+this.major=parseInt(_2c[0])||0;
+this.minor=parseInt(_2c[1])||0;
+this.rev=parseInt(_2c[2])||0;
+};
+com.deconcept.PlayerVersion.prototype.versionIsValid=function(fv){
+if(this.major<fv.major){return false;}
+if(this.major>fv.major){return true;}
+if(this.minor<fv.minor){return false;}
+if(this.minor>fv.minor){return true;}
+if(this.rev<fv.rev){return false;}
+return true;
+};
+com.deconcept.util={getRequestParameter:function(_2e){
+var q=document.location.search||document.location.href.hash;
+if(q){var _30=q.indexOf(_2e+"=");
+var _31=(q.indexOf("&",_30)>-1)?q.indexOf("&",_30):q.length;
+if(q.length>1&&_30>-1){
+return q.substring(q.indexOf("=",_30)+1,_31);}}return "";
+},removeChildren:function(n){
+while(n.hasChildNodes()){
+n.removeChild(n.firstChild);}}};
+if(Array.prototype.push==null){
+Array.prototype.push=function(_33){
+this[this.length]=_33;
+return this.length;};}
+var getQueryParamValue=com.deconcept.util.getRequestParameter;
+var FlashObject=com.deconcept.FlashObject;
