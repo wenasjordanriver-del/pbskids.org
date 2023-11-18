44e4f39d223218fd72311a109e81605272a3a18a
foxtrot5f3s_hideout <141502127+Thecartoondude@users.noreply.github.com>
2023-11-18 11:34:26 -0500

    Create presforaday.pl

---
+++ cgi-registry/democracy/presforaday.pl
@@ -0,0 +1,11 @@
+
+ </div>
+<iframe id="playback" src="/PBS-Kids-2006/cgi-registry/democracy/presforaday.pl" frameborder="0" style="flex:1 0">
+</iframe>
+<script>
+if (window.location.hash) {
+  document.getElementById("playback").src += window.location.hash;
+}
+</script>
+</body>
+</html>
